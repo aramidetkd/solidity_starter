09b18c3c55599e455b743ae849299f3ce48148a5
Ibrahim  Aramide <aramideibrahim2001@gmail.com>
2024-05-05 17:28:13 +0000

    meta craft event challenge 2

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -6,3 +6,39 @@ pragma solidity ^0.8.17;
 contract HelloWorld {
     string public greet = "Hello World!";
 }
+
+contract MessageContextExample {
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function getMessageSender() public view returns (address) {
+        return msg.sender;
+    }
+
+    function getMessageValue() public view returns (uint) {
+        return msg.value;
+    }
+
+    function getMessageData() public view returns (bytes calldata) {
+        return msg.data;
+    }
+
+    function getGasPrice() public view returns (uint) {
+        return tx.gasprice;
+    }
+
+    function getGasLimit() public view returns (uint) {
+        return block.gaslimit;
+    }
+
+    function getBlockTimestamp() public view returns (uint) {
+        return block.timestamp;
+    }
+
+    function getBlockNumber() public view returns (uint) {
+        return block.number;
+    }
+}
